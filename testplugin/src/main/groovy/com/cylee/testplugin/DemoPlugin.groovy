d1e3a934e5f1c1e50461a71675e18f5ce7139433
lichongyang <lichongyang@zuoyebang.com>
2018-08-03 10:03:55 +0100

    添加自定义参数

--- testplugin/src/main/groovy/com/cylee/testplugin/DemoPlugin.groovy
+++ testplugin/src/main/groovy/com/cylee/testplugin/DemoPlugin.groovy
@@ -3,10 +3,19 @@ package com.cylee.testplugin
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
+
+class DemoExtension {
+    String config_test;
+    int config_int;
+
+}
+
 class DemoPlugin implements Plugin<Project> {
     void apply(Project project) {
+        DemoExtension extension = project.extensions.create('democonfig', DemoExtension);
         project.task('hello') {
             doLast {
+                println("read config config_test = $extension.config_test, config_int = $extension.config_int")
                 println "Hello World from the DemoPlugin"
             }
         }
