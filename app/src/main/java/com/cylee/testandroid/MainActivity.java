6402f7345c3135f9cbd324fe5bc5eed620d6e7c4
lichongyang <lichongyang@zuoyebang.com>
2018-08-03 07:49:52 +0100

    init

---
+++ app/src/main/java/com/cylee/testandroid/MainActivity.java
@@ -0,0 +1,11 @@
+package com.cylee.testandroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
