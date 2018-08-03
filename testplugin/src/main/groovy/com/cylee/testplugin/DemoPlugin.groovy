62125468f1d61c9b8185cf74deac31a95464917c
lichongyang <lichongyang@zuoyebang.com>
2018-08-03 10:40:19 +0100

    更新extention

--- testplugin/src/main/groovy/com/cylee/testplugin/DemoPlugin.groovy
+++ testplugin/src/main/groovy/com/cylee/testplugin/DemoPlugin.groovy
@@ -1,13 +1,26 @@
 package com.cylee.testplugin
 
+import org.gradle.api.Action
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
+class Person {
+    String name
+    int age
+}
 
 class DemoExtension {
+    final Person greeter
     String config_test;
     int config_int;
 
+    DemoExtension() {
+        greeter = new Person()
+    }
+
+    void greeter(Action<? super Person> action) {
+        action.execute(greeter)
+    }
 }
 
 class DemoPlugin implements Plugin<Project> {
@@ -15,7 +28,7 @@ class DemoPlugin implements Plugin<Project> {
         DemoExtension extension = project.extensions.create('democonfig', DemoExtension);
         project.task('hello') {
             doLast {
-                println("read config config_test = $extension.config_test, config_int = $extension.config_int")
+                println("read config config_test = $extension.config_test, config_int = $extension.config_int, name = $extension.greeter.name, age = $extension.greeter.age")
                 println "Hello World from the DemoPlugin"
             }
         }
